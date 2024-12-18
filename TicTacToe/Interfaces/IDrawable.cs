5df17a402d671c1e3324a66364f8bbe8a6c13f97
olegbevz <olegdb@inbox.ru>
2024-12-18 18:33:01 +0000

    build fix

---
+++ TicTacToe/Interfaces/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IGraphics.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TicTacToe.Interfaces
+{
+    public interface IDrawable
+    {
+        void Draw(IGraphics graphics, DrawingContext context);
+
+        void Hide(IGraphics graphics, DrawingContext context);
+    }
+}
